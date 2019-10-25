d35e5acda6227d57a7082e5ce44da4d93c0b0ea9
Bret Baker <bretbaker808@gmail.com>
2019-10-25 13:30:22 -0700

    Updates

---
+++ precache-manifest.d1a20425351662487991092108e0573d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb37a9243b3b54d1fbaa2ae0f95b094",
+    "url": "/react-click-game-1/index.html"
+  },
+  {
+    "revision": "3e43464e77d5ec7f13d1",
+    "url": "/react-click-game-1/static/css/main.ba3e88ed.chunk.css"
+  },
+  {
+    "revision": "5410160d7259f198d7db",
+    "url": "/react-click-game-1/static/js/2.d70d7266.chunk.js"
+  },
+  {
+    "revision": "3e43464e77d5ec7f13d1",
+    "url": "/react-click-game-1/static/js/main.1b1bab76.chunk.js"
+  },
+  {
+    "revision": "a7c1d1e67fa558a2140e",
+    "url": "/react-click-game-1/static/js/runtime-main.8c0d66b2.js"
+  }
+]);
